9d916a94812eff8f68ba8c2a9ddf04f4e4cb4104
bulukucing <bulukucing323@gmail.com>
2022-12-24 19:43:28 +0700

    Project page temporary mobile design

--- pages/project.vue
+++ pages/project.vue
@@ -4,22 +4,22 @@
       <NavBar />
       <div>
         <div class="mb-8">
-          <h1 class="text-4xl font-bold">Projects</h1>
-          <p class="text-xl">Beberapa proyek yang saya kerjakan</p>
+          <h1 class="text-xl md:text-4xl font-bold">Projects</h1>
+          <p class="text-md md:text-xl">Beberapa proyek yang saya kerjakan</p>
         </div>
         <div>
           <div class="mb-6">
-            <h2 class="text-2xl font-bold">Personal Project</h2>
+            <h2 class="text-lg md:text-2xl font-bold">Personal Project</h2>
           </div>
           <!-- 
             Desain Sementara..
            -->
           <div>
             <div class="bg-cyan-800 p-6">
-              <h3 class="text-lg font-semibold">Permainan Tabel Periodik</h3>
-              <p>Sebuah game dimana kita menebak nama atom dalam tabel periodik unsur</p>
-              <p>Link: <a href="https://tabelperiodik.netlify.app" class="hover:text-blue-800">tabelperiodik.netlify.app</a></p>
-              <p>Github: <a href="https://github.com/bulukucing/tabelperiodik/" class="hover:text-blue-800">github.com/bulukucing/tabelperiodik</a></p>
+              <h3 class="text-md md:text-lg font-semibold mb-2">Permainan Tabel Periodik</h3>
+              <p class="text-sm md:text-md">Sebuah game dimana kita menebak nama atom dalam tabel periodik unsur</p>
+              <p class="text-sm md:text-md">Link: <a href="https://tabelperiodik.netlify.app" class="hover:text-blue-800">tabelperiodik.netlify.app</a></p>
+              <p class="text-sm md:text-md">Github: <a href="https://github.com/bulukucing/tabelperiodik/" class="hover:text-blue-800">github.com/bulukucing/tabelperiodik</a></p>
             </div>
           </div>
         </div>
